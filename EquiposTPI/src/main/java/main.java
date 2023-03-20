9402666207dae7eb99a52b0553a7e0d47cb40c99
TinoFerrucci <constantinoferrucci@gmail.com>
2023-03-20 16:38:31 -0300

    Update main

--- EquiposTPI/src/main/java/main.java
+++ EquiposTPI/src/main/java/main.java
@@ -1,3 +1,6 @@
+import models.Equipo;
+import models.Partido;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,6 +8,17 @@ import java.nio.file.Paths;
 
 public class main {
     public static void main(String[] args) throws IOException {
+        String[] partidosArr;
+        Equipo equipo1;
+        Equipo equipo2;
+        Partido partido;
+        Path rutaCSV = Paths.get(System.getProperty("user.dir") + "\\EquiposTPI\\src\\main\\java\\resources\\resultados.csv");
+
+        String[] partidos = String.valueOf( Files.readAllLines(rutaCSV) )
+                .replace("[", "")
+                .replace("]", "")
+                .split(",");
+
         /*
         Equipo boquita = new Equipo("Boca", "El mas grande");
         Equipo river = new Equipo("River", "El mas gallina");
@@ -16,24 +30,16 @@ public class main {
 
          */
 
-        Path rutaCSV = Paths.get("C:\\Users\\alexdev\\Documents\\resultados.csv");
 
-        String[] partidos = String.valueOf( Files.readAllLines(rutaCSV) )
-                .replace("[", "")
-                .replace("]", "")
-                .replace(" ", "")
-                .split(",");
 
-        String[] iterar;
-        for(  String p : partidos){
-            iterar = p.split(";");
 
+        for(  String p : partidos) {
+            partidosArr = p.split(";");
+            equipo1 = new Equipo(partidosArr[0], "Equipo 1");
+            equipo2 = new Equipo(partidosArr[3], "Equipo 2");
+            partido = new Partido(equipo1, equipo2, Integer.parseInt(partidosArr[1]), Integer.parseInt(partidosArr[2]));
+            System.out.println(partido);
+            System.out.println(partido.resultado(equipo1));
         }
-
-
-
-
-
-
     }
 }
