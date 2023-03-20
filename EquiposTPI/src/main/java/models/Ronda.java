8ed764bf7a74673c29518e3004444dc73fd72116
TinoFerrucci <constantinoferrucci@gmail.com>
2023-03-20 14:44:48 -0300

    Create Classes

---
+++ EquiposTPI/src/main/java/models/Ronda.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.List;
+
+public class Ronda {
+    private String nro;
+    private List<Partido> partidos;
+
+    public Ronda(String nro, List<Partido> partidos) {
+        this.nro = nro;
+        this.partidos = partidos;
+    }
+
+    public String getNro() {
+        return nro;
+    }
+
+    public void setNro(String nro) {
+        this.nro = nro;
+    }
+
+    public List<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(List<Partido> partidos) {
+        this.partidos = partidos;
+    }
+    public int puntos() {
+        return 0;
+    }
+}
