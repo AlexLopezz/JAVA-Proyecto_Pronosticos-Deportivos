c4311770b10b8881a34c8199c1ee172dd7c9867a
TinoFerrucci <constantinoferrucci@gmail.com>
2023-04-03 17:06:22 -0300

    add SQL Scripts

---
+++ development/src/main/java/resources/files/entrega3/scriptsSQL/addValuesIntoTables.sql
@@ -0,0 +1,99 @@
+use railway;
+
+insert into Persona (nombre)
+values 
+('Tino'),
+('Alex'),
+('Juan');
+
+insert into Equipo (nombre)
+values 
+('Argentina'),
+('Arabia Saudita'),
+('Mexico'),
+('Polonia'),
+('Paises Bajos'),
+('Estados Unidos'),
+('Australia'),
+('Croacia'),
+('Brasil'),
+('Francia'),
+('Marruecos');
+
+
+insert into Fase (descripcion)
+values
+('Fase de grupos'),
+('Fase eliminatoria');
+
+insert into Ronda (descripcion)
+values 
+('Primera fecha'),
+('Segundo fecha'),
+('Tercer fecha'),
+('Octavos de final'),
+('Cuartos de final'),
+('Semifinal'),
+('Final y Tercer puesto');
+
+insert into Partido(equipo1_FK, equipo2_FK, golesEquipo1, golesEquipo2, fase_FK, ronda_FK)
+values
+('Argentina', 'Arabia Saudita', 1, 2, 1, 1),
+('Mexico', 'Polonia', 0, 0, 1, 1),
+('Polonia', 'Arabia Saudita', 2, 0, 1, 2),
+('Argentina', 'Mexico', 2, 0, 1, 2),
+('Polonia', 'Argentina', 0, 2, 1, 3),
+('Arabia Saudita', 'Mexico', 1, 2, 1, 3),
+('Paises Bajos', 'Estados Unidos', 3, 1, 2, 4),
+('Argentina', 'Australia', 2, 1, 2, 4),
+('Paises Bajos', 'Argentina', 3, 4, 2, 5),
+('Croacia', 'Brasil', 4, 2, 2, 5),
+('Argentina', 'Croacia', 3, 0, 2, 6),
+('Francia', 'Marruecos', 2, 0, 2, 6),
+('Argentina', 'Francia', 4, 2, 2, 7),
+('Croacia', 'Marruecos', 2, 1, 2, 7);
+
+insert into Pronostico(partido_FK, equipo_FK, resultado, persona_FK)
+values
+(1, 'Argentina', 'Ganador', 1),
+(2, 'Mexico', 'Ganador', 1),
+(3, 'Polonia', 'Ganador', 1),
+(4, 'Argentina', 'Ganador', 1),
+(5, 'Polonia', 'Ganador', 1),
+(6, 'Arabia Saudita', 'Ganador', 1),
+(7, 'Paises Bajos', 'Ganador', 1),
+(8, 'Argentina', 'Empate', 1),
+(9, 'Paises Bajos', 'Empate', 1),
+(10, 'Croacia', 'Empate', 1),
+(11, 'Argentina', 'Perdedor', 1),
+(12, 'Francia', 'Empate', 1),
+(13, 'Argentina', 'Empate', 1),
+(14, 'Croacia', 'Empate', 1),
+(1, 'Argentina', 'Empate', 2),
+(2, 'Mexico', 'Empate', 2),
+(3, 'Polonia', 'Perdedor', 2),
+(4, 'Argentina', 'Empate', 2),
+(5, 'Polonia', 'Empate', 2),
+(6, 'Arabia Saudita', 'Empate', 2),
+(7, 'Paises Bajos', 'Ganador', 2),
+(8, 'Argentina', 'Perdedor', 2),
+(9, 'Paises Bajos', 'Empate', 2),
+(10, 'Croacia', 'Ganador', 2),
+(11, 'Argentina', 'Perdedor', 2),
+(12, 'Francia', 'Empate', 2),
+(13, 'Argentina', 'Ganador', 2),
+(14, 'Croacia', 'Empate', 2),
+(1, 'Argentina', 'Perdedor', 3),
+(2, 'Mexico', 'Perdedor', 3),
+(3, 'Polonia', 'Perdedor', 3),
+(4, 'Argentina', 'Perdedor', 3),
+(5, 'Polonia', 'Ganador', 3),
+(6, 'Arabia Saudita', 'Ganador', 3),
+(7, 'Paises Bajos', 'Ganador', 3),
+(8, 'Argentina', 'Ganador', 3),
+(9, 'Paises Bajos', 'Ganador', 3),
+(10, 'Croacia', 'Ganador', 3),
+(11, 'Argentina', 'Empate', 3),
+(12, 'Francia', 'Ganador', 3),
+(13, 'Argentina', 'Ganador', 3),
+(14, 'Croacia', 'Ganador', 3);
---
+++ development/src/main/java/resources/files/entrega3/scriptsSQL/createTables.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Persona(
+idPersona int AUTO_INCREMENT primary key,
+nombre varchar(20) not null,
+puntaje int default 0
+);
+
+CREATE TABLE Fase(
+idFase int AUTO_INCREMENT primary key,
+descripcion varchar(30)
+);
+
+CREATE TABLE Ronda(
+idRonda int AUTO_INCREMENT primary key,
+descripcion varchar(30)
+);
+
+CREATE TABLE Equipo(
+Nombre varchar(30) PRIMARY KEY,
+descripcion varchar(50) DEFAULT ''
+);
+
+CREATE TABLE Partido(
+idPartido int auto_increment primary key,
+equipo1_FK varchar(30) not null,
+equipo2_FK varchar(30) not null,
+golesEquipo1 int default 0,
+golesEquipo2 int default 0,
+fase_FK int not null,
+ronda_FK int not null,
+FOREIGN KEY (equipo1_FK) references Equipo(Nombre),
+FOREIGN KEY (equipo2_FK) references Equipo(Nombre),
+FOREIGN KEY (fase_FK) references Fase(idFase),
+FOREIGN KEY (ronda_FK) references Ronda(idRonda)
+);
+
+CREATE TABLE Pronostico(
+idPronostico int auto_increment primary key,
+partido_FK int not null,
+equipo_FK varchar(30) not null,
+resultado enum('Ganador', 'Empate', 'Perdedor') not null,
+persona_FK int not null,
+FOREIGN KEY (partido_FK) references Partido(idPartido),
+FOREIGN KEY (equipo_FK) references Equipo(Nombre),
+FOREIGN KEY (persona_FK) references Persona(idPersona)
+);
+
+CREATE TABLE PuntajeRonda(
+idRonda int primary key,
+idPersona int primary key,
+puntaje int default 0
+);
